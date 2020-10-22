d6039f83332ba3b2029cbdee7c4cb52d205c9881
Jure Stabuc <jure.stabuc@gmail.com>
2020-10-22 11:54:33 +0100

    column change over time

---
+++ src/change-over-time/column/config.js
@@ -0,0 +1,18 @@
+export default
+  {
+    bind: '.time',
+  	margin: {
+  		top: 50,
+  		right: 50,
+  		bottom: 100,
+  		left: 100
+  	},
+  	width: 500,
+  	height: 500,
+    timeParseFormat: '%Y',
+    dateFormat: "'%y",
+  	barFill: '#54a2eb',
+    title: 'Employment in small pubs, by numbers of employees per pub, UK',
+    sortData: true,
+    yLabel: ''
+  }
