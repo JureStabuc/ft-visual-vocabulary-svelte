1c8e347c8558859b83a07c502382d70579426b9f
Jure Stabuc <jure.stabuc@gmail.com>
2020-11-18 15:51:12 +0000

    simple nav bar

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,13 @@
 // import data from './part-to-whole/stacked-bar/data.js'
 // import Order from './ranking/column/Column.svelte'
 // import Stacked from './part-to-whole/stacked-bar/Stacked.svelte'
+import Navbar from './Navbar.svelte'
 </script>
 
 <style></style>
 
 <div class='wrapper'>
+	<div><Navbar/></div>
 	<!-- <div class='magnitude'><Column data={data}/></div>
 	<div class='ranking'><Order data={data}/></div> -->
 	<!-- <div class='stacked'><Stacked data={data}/></div> -->
